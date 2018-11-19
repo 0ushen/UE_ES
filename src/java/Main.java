b839f00f77416e810d4807884cb2968981fe1174
0ushen <samir.benlafya@gmail.com>
2018-11-19 16:23:43 +0100

    + date management

--- src/java/Main.java
+++ src/java/Main.java
@@ -14,26 +14,41 @@ import java.util.ArrayList;
 /**
  *
  * @author Samir
+ * 
+ * 
+ * TODO : 
+ * 
  */
+
+
 public class Main {
     
     public static void main(String[] args){
             
         PersonDao pDao = new PersonDao();
         ArrayList<Person> persons = pDao.load();
+        ArrayList<Person> toDelete = new ArrayList();
 
         for(Person person : persons) {
-        System.out.println("Prenom : " + person.getFirstName() + " | Nom : " 
-                + person.getLastName() + " | Email : " + person.getEmail() + 
-                " | Date de naissance : " + person.getDateOfBirth());
+            System.out.println("Prenom : " + person.getFirstName() + " | Nom : " 
+                    + person.getLastName() + " | Email : " + person.getEmail() + 
+                    " | Date de naissance : " + person.getDateOfBirth());
+            
+            if(person.getFirstName().equals("Samir")){
+                toDelete.add(person);
+            }
+        }
+        
+        for(Person person : toDelete){
+            pDao.delete(person);
         }
         
         
-        Person test = new Person(null, "Samir", "Benlafya", "Belgium", "Liege",
+        /*Person test = new Person(null, "Samir", "Benlafya", "Belgium", "Liege",
                 "4000", "1b, rue des Rivageois", LocalDate.of(1991, 06, 04),
                 "samir.benlafya@gmail.com", false);
         
-        pDao.save(test);
+        pDao.save(test);*/
         
         //pDao.delete(2652);
         
